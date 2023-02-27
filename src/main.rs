6115ff4d5a77aa3c0aa3c907d806b18ca7b9e4eb
Imhegemon2001 <119895956+Imhegemon2001@users.noreply.github.com>
2023-02-26 21:46:47 -0700

    Me? Gongaga

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(LdtkPlugin)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(LdtkWorldBundle::default());
 }
